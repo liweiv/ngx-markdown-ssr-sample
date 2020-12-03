b3d2526e321620679c61dfe6da0e998762cf5eb2
veily <drmfly.liw@gmail.com>
2020-12-03 09:20:55 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-markdown-ssr-sample';
+  markdown = `## Markdown __rulez__!
+  ---
+
+  ### Syntax highlight
+  \`\`\`typescript
+  const language = 'typescript';
+  \`\`\`
+
+  ### Lists
+  1. Ordered list
+  2. Another bullet point
+    - Unordered list
+    - Another unordered bullet point
+
+
+  ### Blockquote
+  > Blockquote to the max :heart:
+
+  ### KaTeX
+
+  \$e=mc^2\$
+
+  \$\\pm\\sqrt{a^2 + b^2}\$
+  `;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, SecurityContext } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MarkdownModule } from 'ngx-markdown';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    MarkdownModule.forRoot({
+      sanitize: SecurityContext.NONE,
+    })
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
